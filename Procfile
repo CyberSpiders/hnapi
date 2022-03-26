641d4318330046ebd385aa0eded4990f668abb87
GNVageesh <vageeshgn2005@gmail.com>
2022-03-26 10:46:50 +0530

    Auto configs

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hnapi.wsgi
+web: python manage.py runserver 0.0.0.0:\$PORT
--- hnapi/settings.py
+++ hnapi/settings.py
@@ -136,3 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
